fb534e06ab37cb5b6cc2a21d913afeb65cafcbac
Youssef ELASERY <joseph.elasery@gmail.com>
2023-05-31 16:20:21 +0100

    Improve Terraform scripts for AKS

--- K8s-Deployment/output.tf
+++ K8s-Deployment/output.tf
@@ -6,13 +6,3 @@ output "aks_fqdn" {
 output "aks_node_rg" {
   value = azurerm_kubernetes_cluster.aks.node_resource_group
 }
-
-
-#acces to k8s cluster 
-resource "local_file" "kubeconfig" {
-  depends_on = [
-    azurerm_kubernetes_cluster.aks
-  ]
-  filename = var.kube_path
-  content  = azurerm_kubernetes_cluster.aks.kube_config_raw
-}
--- K8s-Deployment/variables.tf
+++ K8s-Deployment/variables.tf
@@ -1,33 +1,34 @@
 variable "resource_group_name" {
-  type = string
-
+  description = "The name of the Azure Resource Group where the AKS cluster and ACR will be created."
+  type        = string
+  default     = "AKS-resource-group"
 }
 
 variable "location" {
-  type = string
-  
+  description = "The Azure region where the AKS cluster and ACR will be deployed."
+  type        = string
+  default     = "West Europe"
 }
 
 variable "cluster_name" {
-  type = string
-
+  description = "The name of the AKS cluster to be created."
+  type        = string
+  default     = "aks-cluster"
 }
 
 variable "kubernetes_version" {
-  type = string
-
+  description = "The version of Kubernetes to be used for the AKS cluster."
+  type        = string
+  default     = "1.26.3"
 }
 
 variable "worker" {
-  type = number
+  description = "The number of worker nodes to be provisioned in the AKS cluster."
+  type        = number
+  default     = 3
 }
+
 variable "acr_name" {
-  type = string
-}
-
-
-variable "kube_path" {
+  description = "The name of the Azure Container Registry (ACR) to be created."
   type        = string
-  description = "path to store kube config file "
-  default     = "C:\\Users\\abdelali\\.kube\\config"
 }
